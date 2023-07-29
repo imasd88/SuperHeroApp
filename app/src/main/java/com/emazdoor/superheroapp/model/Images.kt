a0eaff5be313b1e7713df774b350aaf93bfefc25
Ahmed Durrani <demha.ahmed@gmail.com>
2023-07-29 21:50:27 +1000

    add data classes

---
+++ app/src/main/java/com/emazdoor/superheroapp/model/Images.kt
@@ -0,0 +1,8 @@
+package com.emazdoor.superheroapp.model
+
+data class Images(
+    val xs: String,
+    val sm: String,
+    val md: String,
+    val lg: String
+)
---
+++ app/src/main/java/com/emazdoor/superheroapp/model/Work.kt
@@ -0,0 +1,6 @@
+package com.emazdoor.superheroapp.model
+
+data class Work(
+    val occupation: String,
+    val base: String
+)
