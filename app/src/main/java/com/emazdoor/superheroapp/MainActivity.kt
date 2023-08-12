2493f063974c6a9d50d37e4eabc038eef0f0f02b
Ahmed Durrani <demha.ahmed@gmail.com>
2023-08-12 15:44:22 +1000

    hook SuperItemView to LazyColumn

--- app/src/main/java/com/emazdoor/superheroapp/MainActivity.kt
+++ app/src/main/java/com/emazdoor/superheroapp/MainActivity.kt
@@ -4,43 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.emazdoor.superheroapp.ui.components.SuperHeroItemView
 import com.emazdoor.superheroapp.ui.theme.SuperHeroAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            data class Test( val title: String, val subTitle: String)
+            val aList = remember{ mutableStateListOf<Test>() }
+            aList.add(Test("harry", "potter"))
+            aList.add(Test("ABC", "SDSF"))
             SuperHeroAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                LazyColumn(){
+                    items(aList){
+                        SuperHeroItemView(title = it.title, subTitle = it.subTitle)
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    data class Test( val title: String, val subTitle: String)
+    val aList = remember{ mutableStateListOf<Test>() }
+    aList.add(Test("harry", "potter"))
+    aList.add(Test("ABC", "SDSF"))
     SuperHeroAppTheme {
-        Greeting("Android")
+        LazyColumn(){
+           items(aList){
+               SuperHeroItemView(title = it.title, subTitle = it.subTitle)
+           }
+
+        }
     }
 }
--- app/src/main/java/com/emazdoor/superheroapp/ui/components/SuperHeroItemView.kt
+++ app/src/main/java/com/emazdoor/superheroapp/ui/components/SuperHeroItemView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,25 +17,36 @@ import androidx.compose.ui.unit.dp
 import com.emazdoor.superheroapp.R
 
 @Composable
-fun ItemView() {
-    Row(modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically) {
-        CircularBackground(modifier = Modifier.size(48.dp).padding(4.dp))
+fun SuperHeroItemView(title: String = "", subTitle: String = "") {
+    Divider(modifier = Modifier.padding(start = 10.dp), color = Color.LightGray)
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CircularBackground(
+            modifier = Modifier
+                .size(48.dp)
+                .padding(4.dp)
+        )
         {
             Image(
                 painter = painterResource(id = R.drawable.placeholder_image),
                 contentDescription = "circular image"
             )
         }
-        Column( modifier = Modifier.weight(1f).padding(start = 4.dp)) {
-            TextView(text = "Batman", color = Color.Red)
-            TextView(text = "Harry Potter")
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+        ) {
+            TextView(text = title, color = Color.Red)
+            TextView(text = subTitle)
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewItemView (){
-    ItemView()
+fun PreviewItemView() {
+    SuperHeroItemView(title = "Batman", subTitle = "HarryPotter")
 }
